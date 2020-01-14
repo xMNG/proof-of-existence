18d1477c5a1f3363471ccb257bdddc6a4d5a401d
MNG <17662533+xMNG@users.noreply.github.com>
2020-01-13 21:12:14 -0500

    got the react drop zone to work

--- app/src/UploadFile.js
+++ app/src/UploadFile.js
@@ -1,7 +1,8 @@
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 
 const useStyles = makeStyles(theme => ({
@@ -15,31 +16,78 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-function IPFSUpload() {
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+};
 
-}
+const activeStyle = {
+    borderColor: '#2196f3'
+};
 
-const UploadFile = (props) => {
+const acceptStyle = {
+    borderColor: '#00e676'
+};
+
+const rejectStyle = {
+    borderColor: '#ff1744'
+};
+
+
+const UploadFile = () => {
     const classes = useStyles();
-    // TODO: get the upload hooked up to IPFS
+    const [IPFSFile, setIPFSFile] = React.useState(null);
+
+    const onDrop = useCallback(
+        (acceptedFiles) => {
+            console.log('>>>', acceptedFiles[0])
+            setIPFSFile(acceptedFiles[0])
+        },
+        [],
+    )
+
+    const { getRootProps, getInputProps, isDragActive, isDragAccept,
+        isDragReject } = useDropzone({
+            onDrop,
+            multiple: false,
+        });
+
+    const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isDragActive ? activeStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+    }), [
+        isDragActive,
+        isDragReject
+    ]);
+
+    function IPFSUpload() {
+        console.log(IPFSFile);
+    }
+
     return (
         <Container maxWidth='md'>
-            <input
-                accept="*"
-                className={classes.input}
-                id="outlined-button-file"
-                multiple
-                type="file"
-            />
-            <label htmlFor="outlined-button-file">
-                Select file For IPFS Upload<spam>  </spam>
-                <Button variant="outlined" component="span">
-                    Upload
-                </Button>
-            </label>
-            <div>
-                <Button color="primary" variant="outlined" onClick={() => props.setIPFSHash('unwerked')}>setIPFSHash!</Button>
+            <div {...getRootProps({ style })}>
+                <input {...getInputProps()} />
+                {
+                    isDragActive ?
+                        <p>Drop the files here ...</p> :
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                }
             </div>
+            <Button color="primary" variant="outlined" onClick={() => IPFSUpload()}>Upload to IPFS</Button>
         </Container>
     )
 }

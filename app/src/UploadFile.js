124eaac2c2176096632badbfbfc53db7e2634ead
MNG <17662533+xMNG@users.noreply.github.com>
2020-01-15 00:06:02 -0500

    got initial upload to IPFS working

--- app/src/UploadFile.js
+++ app/src/UploadFile.js
@@ -1,20 +1,12 @@
+import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import ipfsClient from "ipfs-http-client";
 import React, { useCallback, useMemo } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-        },
-    },
-    input: {
-        display: 'none',
-    },
-}));
 
 const baseStyle = {
     flex: 1,
@@ -22,14 +14,15 @@ const baseStyle = {
     flexDirection: 'column',
     alignItems: 'center',
     padding: '20px',
-    borderWidth: 2,
+    margin: '0 0 20px 0',
+    borderWidth: 5,
     borderRadius: 2,
     borderColor: '#eeeeee',
     borderStyle: 'dashed',
     backgroundColor: '#fafafa',
     color: '#bdbdbd',
     outline: 'none',
-    transition: 'border .24s ease-in-out'
+    transition: 'border .24s ease-in-out',
 };
 
 const activeStyle = {
@@ -44,6 +37,17 @@ const rejectStyle = {
     borderColor: '#ff1744'
 };
 
+const buttonStyle = {
+    margin: '10px, 0, 0, 0'
+}
+
+const useStyles = makeStyles(theme => ({
+    Button: {
+        marginRight: theme.spacing(1),
+        marginTop: '20px'
+    },
+}));
+
 
 const UploadFile = () => {
     const classes = useStyles();
@@ -51,8 +55,7 @@ const UploadFile = () => {
 
     const onDrop = useCallback(
         (acceptedFiles) => {
-            console.log('>>>', acceptedFiles[0])
-            setIPFSFile(acceptedFiles[0])
+            setIPFSFile(acceptedFiles)
         },
         [],
     )
@@ -73,21 +76,47 @@ const UploadFile = () => {
         isDragReject
     ]);
 
-    function IPFSUpload() {
-        console.log(IPFSFile);
+    const ipfs = ipfsClient({
+        host: 'ipfs.infura.io',
+        port: '5001',
+        protocol: 'https',
+    });
+
+    async function IPFSUpload() {
+        // TODO: get this hooked up to IPFS and return hash
+
+        if (IPFSFile) {
+            // convert to buffer
+            // const fileReader = new FileReader();
+            // fileReader.readAsArrayBuffer(IPFSFile[0]);
+            // const fileArrayBuffer = fileReader.result;
+            let fileBuffer = await Buffer.from(IPFSFile);
+            console.log(">>>>>: IPFSUpload -> fileBuffer", fileBuffer, typeof fileBuffer)
+            // send to infura
+            const results = await ipfs.add(fileBuffer.buffer);
+            const IPFSHashStr = results[0].hash;
+            console.log(
+                '>>>>>: IPFS -> fileReader.onload -> IPFSHashStr',
+                IPFSHashStr,
+                typeof IPFSHashStr
+            );
+        }
     }
 
     return (
         <Container maxWidth='md'>
-            <div {...getRootProps({ style })}>
-                <input {...getInputProps()} />
-                {
-                    isDragActive ?
-                        <p>Drop the files here ...</p> :
-                        <p>Drag 'n' drop some files here, or click to select files</p>
-                }
-            </div>
-            <Button color="primary" variant="outlined" onClick={() => IPFSUpload()}>Upload to IPFS</Button>
+            <Grid container direction='column' alignContent='space-between'>
+                <div {...getRootProps({ style })}>
+                    <input {...getInputProps()} />
+                    {
+                        isDragActive ?
+                            <p>Drop the files here ...</p> :
+                            <p>Drag 'n' drop some files here, or click to select files</p>
+                    }
+                </div>
+                <span>IPFS File Name: {IPFSFile ? IPFSFile.name : ''}</span>
+                <Button className={classes.Button} disabled={IPFSFile == null} color="primary" variant="outlined" onClick={() => IPFSUpload()}>Upload to IPFS</Button>
+            </Grid>
         </Container>
     )
 }

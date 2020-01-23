65720c4ffc7b79f2b664928e1c1b4ee58e14f541
MNG <17662533+xMNG@users.noreply.github.com>
2020-01-22 23:07:49 -0500

    fixed warnings on displayHashInfo.js

--- app/src/DisplayHashInfo.js
+++ app/src/DisplayHashInfo.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles({
         minWidth: 650,
     },
     tableContainer: {
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19)',
     }
 });
 
